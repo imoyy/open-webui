166d8b343aa4ef7cdbbd21b9639c0394cc726281
moyy <imoyyi@outlook.com>
2024-12-13 12:00:12 +0000

    Update Chat.svelte

--- src/lib/components/chat/Chat.svelte
+++ src/lib/components/chat/Chat.svelte
@@ -2256,7 +2256,7 @@
 				<div class="flex flex-col flex-auto z-9 mx-auto w-full md:max-w-3xl ">
 					{#if $settings?.landingPageMode === 'chat' || createMessagesList(history.currentId).length > 0}
 						<div
-							class="pb-2.5 px-3 lg:px-0 flex flex-col justify-between w-full flex-auto no-scrollbar h-0 z-9"
+							class="pb-2.5 px-3 lg:px-0 flex flex-col justify-between w-full flex-auto overflow-auto no-scrollbar h-0 z-9"
 							id="messages-container"
 							bind:this={messagesContainerElement}
 							on:scroll={(e) => {
